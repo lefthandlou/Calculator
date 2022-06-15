9e004cd8c70de99803ac1e03c21c8151f2e70577
lefthandlou <lulu.bestvater@gmail.com>
2022-06-15 13:03:25 -0400

    Working numberpad listener that logs all input before equals as arrayh and string.

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
-let allButtons = document.querySelectorAll('.button');
+/*
+event listener - decide if using one or multiple
+    would it be num listener, operator listener, num listener?
+
+when clicked, buttons need their value show upon the display;
+if multi-digit numbers are input, that all needs to show up.
+Button clicks need to be stored for use in equation;
+Operator clicks need to pull correct math equation function
+So equals button listener needs to be the final listener that summons all?
+Clear button listener to refresh page and start over
+
+*/
+
+// addEVentListener for each num button; let buttonContent = button.textContent;
+// push to array 
+
+//let a = eventListener 1
+//let b = eventListener 3
+// let function = eL1 (eL2) el3;
+// return product
+
+//TODO set up numpad event listener to concat all numbers (including zeros and
+// periods) clicked before operator; display and store; save as a
+//TODO set up operator listener; display operator chosen, store or give class
+//TODO numpad event again; store as b
+//TODO wire up equals EL to take stored a, b, and operator and summon correct
+//math function, return result
+// also, how do I get the value from the button clicked??
+
+// WHAT I'VE TRIED - NUMPAD EVENT LISTENER
+//      for loop to push choice to array, allowing multiple numbers to concat XX
+
 let numButtons = document.querySelectorAll('.numButton');
-let operatorButtons = document.querySelectorAll('.operator');
 let clearButton = document.querySelector('.clearButton');
-let equalButton = document.querySelector('.equalButton');
-let periodButton = document.querySelector('.period');
 let calcDisplay = document.querySelector('.displayContainer');
+let array = [];
+
+clearButton.addEventListener('click', () => location.reload());
+
+function chooseNumbers() { numButtons.forEach(button => button.addEventListener('click', function handleClick(e){
+        let numberChoice = button.textContent;
+        array.push(numberChoice);
+        console.log(array);
+        let numDisplay = (array.join(''));
+        calcDisplay.textContent = numDisplay;
+        console.log(numDisplay);
+    }))
+};
 
 
-
-
-//set event listener to all buttons
-//log text content of button into display
-//make everything begfore operator "a" and everything after operator "b"
-//combine nums and operators
-//return result
-
-
-function numListener() {
-    allButtons.forEach(button => {
-        button.addEventListener('click', function handleClick(event){
-            calcDisplay.textContent = button.textContent;
-            button.classList.add('clicked');
-            button.setAttribute('id', 'clicked');
-            let buttonText = document.getElementById('click');
-            let array = [];
-            array.push(buttonText.textContent);
-            console.log(array);
-        });
-    });
-}
-
-function equationArray() {
-    let equationElems = document.getElementsByClassName('.clicked');
-    let array = [];
-    array.push(equationElems.textContent);
-    console.log(array);
-}
-
-function clearCalc() {
-    clearButton.addEventListener('click', () => location.reload());
-}
-
-console.log(numListener());
-console.log(clearCalc());
+chooseNumbers();
